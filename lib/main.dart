cd14c87e10a5db20e054ba6a003eabdd1be6c71d
developer-kuntal <kuntal.iiest@gmail.com>
2019-04-12 00:55:00 +0530

    facbook login update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MyMainPage(),
   ));
 }
@@ -21,38 +22,22 @@ class _MyMainPageState extends State<MyMainPage> {
   bool isLogged = false;
   FirebaseUser myUser;
 
-  Future<FirebaseUser> _loginWithFacebook() async {
-    var facebookLogin = new FacebookLogin();
-    var result = await facebookLogin.logInWithReadPermissions(["public_profile","email",]);
+  Future<Null> _loginWithFacebook() async {
 
-    // final token = result.accessToken.token;
-    // final graphResponse = await http.get(
-    //         'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${token}');
-    // final profile = graphResponse.body.toString();
-    // // debugPrint(result.status.toString());
-    // debugPrint(profile);
+    var facebookLogin = new FacebookLogin();
+    var result = await facebookLogin.logInWithReadPermissions(["email"]);
+
+    // print(result.accessToken.token);
 
     final AuthCredential credential = FacebookAuthProvider.getCredential(
       accessToken: result.accessToken.token
     );
 
-    switch (result.status) {
-      case FacebookLoginStatus.loggedIn:
-        final FirebaseUser user = await _auth.signInWithCredential(credential);
-        return user;
-        break;
-      case FacebookLoginStatus.cancelledByUser:
-        print('Cancel by user');
-        break;
-      case FacebookLoginStatus.error:
-        print('Here\'s the error Facebook gave us: ${result.errorMessage}');
-        break;
-      default:
-        return null;
-    }
+    FirebaseUser user = await _auth.signInWithCredential(credential);
+    return user;
   }
 
-  void _logOut() async {
+  Future<void> _logOut() async {
     await _auth.signOut().then((response) {
       isLogged = false;
       setState(() { });
@@ -88,7 +73,22 @@ class _MyMainPageState extends State<MyMainPage> {
             Text("Name: "+ myUser.displayName),
             Image.network(myUser.photoUrl),
           ],
-        ) : FacebookSignInButton(onPressed: _logIn),
+        ) : 
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30.0, 40.0, 20.0, 30.0),
+              child: new Container(
+                child: RaisedButton(
+                  onPressed: _logIn,
+                  child: const Text('Facebook login', 
+                        style: TextStyle(color: Colors.blue, fontSize: 40, 
+                        fontWeight: FontWeight.bold),),
+                ),
+              ),
+            )
+          ],
+        )//FacebookSignInButton(onPressed: _logIn),
       ),
     );
   }
